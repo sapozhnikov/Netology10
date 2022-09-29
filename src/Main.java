97faa4971aa8dc5dd450e12369a4b9393a217573
Dmitriy Sapozhnikov <aurariel@gmail.com>
2022-09-29 21:08:09 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long under18count = persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .count();
+        System.out.println("Несовершеннолетних: " + under18count);
+
+        long conscriptsCount = persons.stream()
+                .filter(p -> p.getAge() >= 18 && p.getAge() <= 27)
+                .filter(p -> p.getSex() == Sex.MAN)
+                .count();
+        System.out.println("Призывников: " + conscriptsCount);
+
+        List<String> lastNamesOfWorkingPpl = persons.stream()
+                .filter(p -> p.getEducation() == Education.HIGHER)
+                .filter(p -> (p.getSex() == Sex.MAN && p.getAge() >= 18 && p.getAge() <= 65) ||
+                        (p.getSex() == Sex.WOMAN && p.getAge() >= 18 && p.getAge() <= 60))
+                .map(Person::getFamily)
+                .sorted()
+                .collect(Collectors.toList());
+        //System.out.println(lastNamesOfWorkingPpl);
+        System.out.println("В отсортированном списке рабочих: " + lastNamesOfWorkingPpl.size());
+    }
+}
